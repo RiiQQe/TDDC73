0ba10ac4226c0b3d7354804b0690edd23c113232
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-11-03 09:48:44 +0100

    Populating adapter

--- Lab2/app/src/main/java/com/tddc73/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/tddc73/lab2/MainActivity.java
@@ -4,22 +4,35 @@ import android.app.ExpandableListActivity;
 import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    ExpandableListView elv;
+    ExpandableListAdapter ela;
+    List<String> listHeader = new ArrayList<String>();
+    HashMap<String, List<String>> listData = new HashMap<String, List<String>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        ExpandableListView elv = (ExpandableListView) findViewById(R.id.list);
+        elv = (ExpandableListView) findViewById(R.id.list);
 
-        ExpandableListAdapter ela = new ExpandableListAdapter() {
+        populate();
+
+        ela = new ExpandableListAdapter() {
             @Override
             public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
             }
 
             @Override
@@ -108,10 +121,29 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        
-
         elv.setAdapter(ela);
 
-        setContentView(R.layout.activity_main);
+    }
+
+    private void populate() {
+        listHeader.add("light");
+        listHeader.add("Medium");
+        listHeader.add("Dark");
+
+        List<String> light = new ArrayList<String>();
+        light.add("yellow");
+        light.add("blue");
+
+        List<String> medium = new ArrayList<String>();
+        medium.add("white");
+        medium.add("gray");
+
+        List<String> dark = new ArrayList<String>();
+        dark.add("black");
+        dark.add("red");
+
+        listData.put(listHeader.get(0), light);
+        listData.put(listHeader.get(1), medium);
+        listData.put(listHeader.get(2), dark);
     }
 }
