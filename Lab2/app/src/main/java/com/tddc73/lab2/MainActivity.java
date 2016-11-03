f31bebf619d2bc2d94ba4f9fd91df25eed83bad3
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-11-03 09:16:00 +0100

    Add adapter and listview in code

--- Lab2/app/src/main/java/com/tddc73/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/tddc73/lab2/MainActivity.java
@@ -1,13 +1,118 @@
 package com.tddc73.lab2;
 
+import android.app.ExpandableListActivity;
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ExpandableListView elv = (ExpandableListView) findViewById(R.id.list);
+
+        ExpandableListAdapter ela = new ExpandableListAdapter() {
+            @Override
+            public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+            }
+
+            @Override
+            public int getGroupCount() {
+                return 0;
+            }
+
+            @Override
+            public int getChildrenCount(int i) {
+                return 0;
+            }
+
+            @Override
+            public Object getGroup(int i) {
+                return null;
+            }
+
+            @Override
+            public Object getChild(int i, int i1) {
+                return null;
+            }
+
+            @Override
+            public long getGroupId(int i) {
+                return 0;
+            }
+
+            @Override
+            public long getChildId(int i, int i1) {
+                return 0;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+                return null;
+            }
+
+            @Override
+            public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+                return null;
+            }
+
+            @Override
+            public boolean isChildSelectable(int i, int i1) {
+                return false;
+            }
+
+            @Override
+            public boolean areAllItemsEnabled() {
+                return false;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public void onGroupExpanded(int i) {
+
+            }
+
+            @Override
+            public void onGroupCollapsed(int i) {
+
+            }
+
+            @Override
+            public long getCombinedChildId(long l, long l1) {
+                return 0;
+            }
+
+            @Override
+            public long getCombinedGroupId(long l) {
+                return 0;
+            }
+        };
+
+        
+
+        elv.setAdapter(ela);
+
         setContentView(R.layout.activity_main);
     }
 }
