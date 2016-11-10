f7e4d3b5689e0b976d155f694edb30305a54998f
MichaelSjostrom <micke_moj@hotmail.com>
2016-11-10 15:47:18 +0100

    added hint ass attribute to the component

--- Lab3/app/src/main/java/com/example/micke/lab3/InteractiveSearcher.java
+++ Lab3/app/src/main/java/com/example/micke/lab3/InteractiveSearcher.java
@@ -1,6 +1,7 @@
 package com.example.micke.lab3;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.EditText;
 
@@ -9,7 +10,7 @@ import android.widget.EditText;
  */
 
 public class InteractiveSearcher extends EditText{
-    private String defaultText;
+    private String hint;
 
     public InteractiveSearcher(Context context) {
         super(context);
@@ -19,6 +20,29 @@ public class InteractiveSearcher extends EditText{
     public InteractiveSearcher(Context context, AttributeSet attrs) {
         super(context, attrs);
         setInteractiveSearcher();
+        // retrieved values correspond to the positions of the attributes
+        TypedArray typedArray = context.obtainStyledAttributes(attrs,
+                R.styleable.InteractiveSearcher);
+        int count = typedArray.getIndexCount();
+
+        try {
+
+            for (int i = 0; i < count; ++i) {
+
+                int attr = typedArray.getIndex(i);
+                // the attr corresponds to the hint attribute
+                if (attr == R.styleable.InteractiveSearcher_hint) {
+                    hint = typedArray.getString(attr);
+                    setHint(hint);
+                }
+            }
+        }
+            // the recycle() will be executed obligatorily
+            finally{
+                // for reuse
+                typedArray.recycle();
+            }
+
     }
 
     public InteractiveSearcher(Context context, AttributeSet attrs, int defStyle) {
@@ -28,7 +52,6 @@ public class InteractiveSearcher extends EditText{
 
     private void setInteractiveSearcher() {
 
-        setHint("Sök för fan");
     }
 
 
