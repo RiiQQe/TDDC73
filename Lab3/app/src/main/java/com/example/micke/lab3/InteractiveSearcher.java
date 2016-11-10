b557cbbbf4ffc49e143eb47ff06aad7331091f7d
MichaelSjostrom <micke_moj@hotmail.com>
2016-11-10 15:37:56 +0100

    added base structure of custom component

---
+++ Lab3/app/src/main/java/com/example/micke/lab3/InteractiveSearcher.java
@@ -0,0 +1,35 @@
+package com.example.micke.lab3;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+/**
+ * Created by micke on 2016-11-10.
+ */
+
+public class InteractiveSearcher extends EditText{
+    private String defaultText;
+
+    public InteractiveSearcher(Context context) {
+        super(context);
+        setInteractiveSearcher();
+    }
+
+    public InteractiveSearcher(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setInteractiveSearcher();
+    }
+
+    public InteractiveSearcher(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setInteractiveSearcher();
+    }
+
+    private void setInteractiveSearcher() {
+
+        setHint("Sök för fan");
+    }
+
+
+}
