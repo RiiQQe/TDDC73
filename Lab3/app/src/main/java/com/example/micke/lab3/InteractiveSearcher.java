9dca2b174a32933e46fedfde5cb17d2c4abb9c2d
MichaelSjostrom <micke_moj@hotmail.com>
2016-11-10 15:56:43 +0100

    previous attrs were redundant

--- Lab3/app/src/main/java/com/example/micke/lab3/InteractiveSearcher.java
+++ Lab3/app/src/main/java/com/example/micke/lab3/InteractiveSearcher.java
@@ -20,29 +20,6 @@ public class InteractiveSearcher extends EditText{
     public InteractiveSearcher(Context context, AttributeSet attrs) {
         super(context, attrs);
         setInteractiveSearcher();
-        // retrieved values correspond to the positions of the attributes
-        TypedArray typedArray = context.obtainStyledAttributes(attrs,
-                R.styleable.InteractiveSearcher);
-        int count = typedArray.getIndexCount();
-
-        try {
-
-            for (int i = 0; i < count; ++i) {
-
-                int attr = typedArray.getIndex(i);
-                // the attr corresponds to the hint attribute
-                if (attr == R.styleable.InteractiveSearcher_hint) {
-                    hint = typedArray.getString(attr);
-                    setHint(hint);
-                }
-            }
-        }
-            // the recycle() will be executed obligatorily
-            finally{
-                // for reuse
-                typedArray.recycle();
-            }
-
     }
 
     public InteractiveSearcher(Context context, AttributeSet attrs, int defStyle) {
