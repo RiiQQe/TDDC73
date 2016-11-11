eee60958982b5806c131e1fe7ea71147b68872b6
MichaelSjostrom <micke_moj@hotmail.com>
2016-11-11 12:23:37 +0100

    testing http connection

--- Lab3/app/src/main/java/com/example/micke/lab3/MainActivity.java
+++ Lab3/app/src/main/java/com/example/micke/lab3/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        InteractiveSearcher interactiveSearcher = (InteractiveSearcher) findViewById(R.id.interactiveSearcher);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,7 +31,31 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        interactiveSearcher.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
     }
+
+    /*private InputStream openHttpConnection(String urlStr{
+        InputStream in = null;
+        int resCode = -1;
+        return;
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
