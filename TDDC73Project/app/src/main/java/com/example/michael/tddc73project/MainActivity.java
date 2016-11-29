0872a168002c153131024c8e8de36f8e3de2cd3c
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-11-29 15:40:46 +0100

    Add constructor for signUpForm

--- TDDC73Project/app/src/main/java/com/example/michael/tddc73project/MainActivity.java
+++ TDDC73Project/app/src/main/java/com/example/michael/tddc73project/MainActivity.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    SignUpForm signUpForm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        signUpForm = (findViewById(R.id.SignUpForm));
+
+        signUpForm.addEditableText("Extra TextField");
+
         setContentView(R.layout.activity_main);
     }
 }
--- TDDC73Project/app/src/main/java/com/example/michael/tddc73project/SignUpForm.java
+++ TDDC73Project/app/src/main/java/com/example/michael/tddc73project/SignUpForm.java
@@ -1,8 +1,23 @@
 package com.example.michael.tddc73project;
 
+import android.content.Context;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
 /**
  * Created by michael on 2016-11-29.
  */
 
-public class SignUpForm {
+public class SignUpForm extends LinearLayout {
+
+    public SignUpForm(Context ctx) {
+        super(ctx);
+
+    }
+
+    public void addEditableText(String hint) {
+        EditText editText = new EditText(getContext());
+
+
+    }
 }
