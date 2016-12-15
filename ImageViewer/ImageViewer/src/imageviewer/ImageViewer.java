3b84f031f46e024c65fb6490a22b6725f2ac48c8
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-12-15 11:17:24 +0100

    Add next and prev image functionallity

--- ImageViewer/ImageViewer/src/imageviewer/ImageViewer.java
+++ ImageViewer/ImageViewer/src/imageviewer/ImageViewer.java
@@ -5,15 +5,20 @@
  */
 package imageviewer;
 
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  *
@@ -21,31 +26,83 @@ import javafx.scene.image.ImageView;
  */
 public class ImageViewer extends Application {
     
+    private static int currIndex;
+    
     @Override
     public void start(Stage primaryStage) {
         GridPane root = new GridPane();
         
-        Image image = new Image("A.png");
+        currIndex = 0;
+        
+        Image image1 = new Image("A.png");
+        Image image2 = new Image("B.png");
+        Image image3 = new Image("K.png");
+        
+        ArrayList<Image> imageList = new ArrayList<Image>();
+        
+        imageList.add(image1);
+        imageList.add(image2);
+        imageList.add(image3);
         
         ImageView iv1 = new ImageView();
-        iv1.setImage(image);
-        iv1.setFitWidth(100);
+        iv1.setImage(image1);
+        iv1.setFitWidth(200);
         iv1.setPreserveRatio(true);
         iv1.setSmooth(true);
         iv1.setCache(true);
         
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+        Button nextBtn = new Button();
+        nextBtn.setText(">");
+        nextBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                currIndex++;
+                currIndex = currIndex > imageList.size() - 1 ? 0 : currIndex;
+                
+                iv1.setImage(imageList.get(currIndex));
             }
         });
         
-        root.getChildren().add(iv1);
-        root.getChildren().add(btn);
+        Button prevBtn = new Button();
+        prevBtn.setText("<");
+        prevBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                currIndex--;
+                currIndex = currIndex < 0 ? imageList.size() - 1 : currIndex;
+                
+                iv1.setImage(imageList.get(currIndex));
+            }
+        });
+        
+        Button scaleBtn = new Button();
+        scaleBtn.setText("sc");
+        scaleBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //TODO: DO STUFF LIKE ROTATE / SCALE
+                System.out.println("Scale image");
+            }
+        });
+        
+        Button rotateBtn = new Button();
+        rotateBtn.setText("rt");
+        rotateBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //TODO: DO STUFF LIKE ROTATE / SCALE
+                System.out.println("Rotate image");
+            }
+        });
+        
+        HBox btnGroup = new HBox();
+        
+        btnGroup.setSpacing(0); 
+        btnGroup.getChildren().addAll(prevBtn, scaleBtn, rotateBtn, nextBtn);
+        root.setAlignment(Pos.CENTER);
+        
+        root.add(iv1, 0, 0);
+        root.add(btnGroup, 0, 1);
         
         Scene scene = new Scene(root, 300, 250);
         
